726ee01eabb8ba2b678f54431ae6d70154680bf3
esqleto <esqueletoncraft@gmail.com>
2024-05-09 17:50:20 -0300

    fist commit

--- Dice flipper/DiceFlipper.py
+++ Dice flipper/DiceFlipper.py
@@ -1,4 +1,7 @@
 import math
 
+#Starts with 100 dices on a tray all with the value of 1.
+#A force is applied to the tray, and causes the a portion of the dice to be flipped according to it.
+#The amount flipped isn't always the same, there needs to be a probability of there being more or less flipped dices.
+#The value of all dices must them be 
 
-print("Hello World")
