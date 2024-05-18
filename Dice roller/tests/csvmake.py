f28a8e43dc3072c0ba845752d52c5116529e8804
esqleto <esqueletoncraft@gmail.com>
2024-05-18 10:22:26 -0300

    testing CSV file making

---
+++ Dice roller/tests/csvmake.py
@@ -0,0 +1,12 @@
+import csv
+
+#data = [[10,20,30],[40,50,60],[70,80,90]]
+
+file = open("CSVFile.csv", "w")
+
+writer = csv.writer(file)
+
+for row in data:
+    print(row)
+    writer.writerow(row)
+file.close()
