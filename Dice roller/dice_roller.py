a0c8a6c4ec81ca360e0e3806e4c55f1c52b28b0d
esqleto <esqueletoncraft@gmail.com>
2024-05-21 21:30:03 -0300

    added general statistics to the document

--- Dice roller/dice_roller.py
+++ Dice roller/dice_roller.py
@@ -1,6 +1,6 @@
-# Note: A lot of the comments are in portuguese because this is a university assignment.
-# They will be removed later on.
-import math
+# Note: A lot of the text is in portuguese because this is a university assignment.
+# It will be changed later on.
+import statistics
 import random
 import csv
 
@@ -13,8 +13,10 @@ while True:
     force = float(input("Digite a força aplicada(Entre 0.1 e 10): "))
     if force >= 0.1 and force <= 10:
         break
-
-times = int(input("Digite o número de balanços: "))
+while True:
+    times = int(input("Digite o número de balanços: "))
+    if times > 0:
+        break
 
 dices = [[1] * times] * 100
 dice_sum = [[0] * times] * 100
@@ -25,7 +27,6 @@ rolled = round((force / 10) * 100)
 header = ["|Dados|"]
 for i in range(times):
     header += ["|Balanço-" + str(i + 1) + "|"]
-
 writer.writerow(header)
 
 for row in range(100):
@@ -42,8 +43,6 @@ for row in range(100):
             else:
                 dice_sum[row][dice] += dices[row][dice]
                 read[row][dice] = str(dices[row][dice]) + " | " + str(dices[row][dice])
-
-            print(dices[row])
     else:
         dices[row] = [1] * times
         for rest in range(times):
@@ -53,6 +52,9 @@ for row in range(100):
     writer.writerow(["Dado-" + str(row + 1)] + read[row])
 
 writer.writerow(["|Somas|"] + dice_sum[99])
+writer.writerow(["|Média|"] + [statistics.median(dice_sum[99])])
+writer.writerow(["|Mediana|"] + [statistics.mean(dice_sum[99])])
+writer.writerow(["|Moda|"] + [statistics.mode(dice_sum[99])])
 
 for i in range(times):
     writer_chart.writerow(["Soma do balanço:" + str(i + 1)] + [dice_sum[99][i]])
